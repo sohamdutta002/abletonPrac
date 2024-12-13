0577d0056448fa18c722ba9175ee1fff3c709d4e
Soham Dutta <duttasoham2222@gmail.com>
2024-12-13 16:20:35 +0530

    about1 update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './Components/Home'
 function App() {
 
   return (
-    <div  className='bg-[#f2f2f2] w-[100vw] h-[100vh] flex justify-center'>
+    <div  className='bg-[#f2f2f2] w-[auto] h-[auto] flex justify-center'>
       <Home/>
     </div> 
   )
